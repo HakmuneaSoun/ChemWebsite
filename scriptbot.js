0cbd97303693de53a891260c08414236183b9f81
HakmuneaSoun <hakmunea@gmail.com>
2026-05-12 01:19:09 +0700

    Update API key management and .env handling

--- scriptbot.js
+++ scriptbot.js
@@ -8,6 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
+const API_KEY = import.meta.env.API_KEY;
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`;
 
 const userData = {
