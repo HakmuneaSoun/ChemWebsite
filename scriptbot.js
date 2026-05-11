7383cbc6a0a98db9b9ae5182e2e4802e1799e56a
HakmuneaSoun <hakmunea@gmail.com>
2026-05-12 01:10:22 +0700

    Upating .gitignore and scriptbot.js with API key.

--- scriptbot.js
+++ scriptbot.js
@@ -8,7 +8,6 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyCd3rR0hRBd5GUAnCBOwKeTCdflTAXA3U8";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`;
 
 const userData = {
